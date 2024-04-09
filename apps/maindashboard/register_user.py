3368feb81f2e0d24f1e2719b77eb4401b6bcf3d4
Naomi Takagaki <natakagaki@up.edu.ph>
2024-04-10 02:30:58 +0800

    tapos na din sawakaaassss

--- apps/maindashboard/register_user.py
+++ apps/maindashboard/register_user.py
@@ -1,7 +1,7 @@
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
-import dash_table
+from dash import dash_table
 import dash
 from dash.dependencies import Input, Output, State
 from dash.exceptions import PreventUpdate
@@ -11,6 +11,21 @@ from apps import commonmodules as cm
 from app import app
 from apps import dbconnect as db
 
+import bcrypt
+import base64
+import hashlib
+
+def hash_password(password):
+    # Convert the password to bytes if it's a string
+    password_bytes = password.encode('utf-8')
+
+    # Generate a salt
+    salt = bcrypt.gensalt()
+
+    # Generate the hashed password
+    hashed_password = bcrypt.hashpw(password_bytes, salt)
+
+    return hashed_password
 
 form = dbc.Form(
     [
@@ -26,9 +41,9 @@ form = dbc.Form(
         ),
         dbc.Row(
             [
-                dbc.Label("Middle Initial", width=4),
+                dbc.Label("Middle Name", width=4),
                 dbc.Col(
-                    dbc.Input(type="text", id='user_midinitial'),
+                    dbc.Input(type="text", id='user_mname'),
                     width=6,
                 ),
             ],
@@ -65,8 +80,8 @@ form = dbc.Form(
                     dbc.Select(
                         id='user_sex',
                         options=[ 
-                            {'label': 'Female', 'value': 'Female'},
-                            {'label': 'Male', 'value': 'Male'}, 
+                            {'label': 'Female', 'value': '2'},
+                            {'label': 'Male', 'value': '1'}, 
                         ]
                     ),
                     width=6,
@@ -98,7 +113,7 @@ form = dbc.Form(
             [
                 dbc.Label("ID Number", width=4),
                 dbc.Col(
-                    dbc.Input(type="number", id='user_id_num'),
+                    dbc.Input(type="text", id='user_id_num'),
                     width=6,
                 ),
             ],
@@ -106,13 +121,21 @@ form = dbc.Form(
         ),
         dbc.Row(
             [
-                dbc.Label("Office / Department", width=4),
+                dbc.Label("Office", width=4),
                 dbc.Col(
                     dbc.Select(
                         id='user_office',
                         options=[ 
-                            {'label': 'Department 1', 'value': '1'},
-                            {'label': 'Department 2', 'value': '2'}, 
+                            {'label': "Quality Assurance Office", 'value': '1'},
+                            {'label': "Office of the Vice President for Academic Affairs", 'value': '2'},
+                            {'label': "Office of the Chancellor", 'value': '3'},
+                            {'label': "Office of the Vice Chancellor for Academic Affairs", 'value': '4'},
+                            {'label': "Office of the Vice Chancellor for Research and Development", 'value': '5'},
+                            {'label': "Office of the Vice Chancellor for Administration", 'value': '6'},
+                            {'label': "Office of the Vice Chancellor for Student Affairs", 'value': '7'},
+                            {'label': "Office of the Vice Chancellor for Community Affairs", 'value': '8'},
+                            {'label': "Office of the Vice Chancellor for Planning and Development", 'value': '9'},
+                            {'label': "UP Diliman Information Office (UPDIO)", 'value': '10'}
                         ]
                     ),
                     width=6,
@@ -136,7 +159,7 @@ form = dbc.Form(
             [
                 dbc.Label("Email Address", width=4),
                 dbc.Col(
-                    dbc.Input(type="email", id='user_email'),
+                    dbc.Input(type="text", id='user_email'),
                     width=6,
                 ),
             ],
@@ -153,6 +176,17 @@ form = dbc.Form(
             ],
             className="mb-2",
         ),
+ 
+        dbc.Row(
+            [
+                dbc.Label("Confirm Password", width=4),
+                dbc.Col(
+                    dbc.Input(type="password", id = 'confirm_password', placeholder = 'Confirm password' ),
+                    width=6,
+                ),
+            ],
+            className="mb-2",
+        ),
 
         # Access Type
         dbc.Row(
@@ -162,8 +196,8 @@ form = dbc.Form(
                     dbc.Select(
                         id='user_access_type',
                         options=[ 
-                            {'label': 'Basic Access', 'value': 'Basic Access'},
-                            {'label': 'Full Access', 'value': 'Full Access'}, 
+                            {'label': 'Basic Access', 'value': '1'},
+                            {'label': 'Full Access', 'value': '2'}, 
                         ]
                     ),
                     width=6,
@@ -172,23 +206,9 @@ form = dbc.Form(
             className="mb-2",
         ),
 
-        dbc.Row(
-            [
-                dbc.Label("Account Status", width=4),
-                dbc.Col(
-                    dbc.Select(
-                        id='user_acc_status',
-                        options=[ 
-                            {'label': 'Active', 'value': 'Active'},
-                            {'label': 'Suspended', 'value': 'Suspended'},  
-                            {'label': 'Deleted', 'value': 'Deleted'}, 
-                        ]
-                    ),
-                    width=6,
-                ),
-            ],
-            className="mb-2",
-        ),
+        
+        
+ 
  
  
         html.Br(),
@@ -247,6 +267,7 @@ layout = html.Div(
                     html.Hr(),
                     dbc.Alert(id='registeruser_alert', is_open=False), # For feedback purpose
                     form,
+                     
                 ],
                 width=8, style={'marginLeft': '15px'}
                 
@@ -264,78 +285,120 @@ layout = html.Div(
 )
 
 
+
+
+
 @app.callback(
     [
-         
         Output('registeruser_alert', 'color'),
         Output('registeruser_alert', 'children'),
         Output('registeruser_alert', 'is_open'),
-         
         Output('registeruser_successmodal', 'is_open')
     ],
-    [ 
+    [
         Input('save_button', 'n_clicks')
     ],
     [
-    
         State('user_fname', 'value'),
-        State('user_midinitial', 'value'),
+        State('user_mname', 'value'),
         State('user_sname', 'value'),
         State('user_livedname', 'value'),
-        State('user_sex', 'value'),  # You might want to handle this as a Dropdown or RadioItems
+        State('user_sex', 'value'),    
         State('user_bday', 'date'),
         State('user_phone_num', 'value'),
         State('user_id_num', 'value'),
-        State('user_office', 'value'),  # This might be a Dropdown linked to your offices table
+        State('user_office', 'value'),
         State('user_position', 'value'),
         State('user_email', 'value'),
-        State('user_password', 'value'),  # Ensure this is handled securely
-        State('user_access_type', 'value'),  # Dropdown linked to your access_type table
-        State('user_acc_status', 'value'),  # Dropdown linked to your account_status table
-        State('user_profile_pic', 'value')  # This would require handling file upload
+        State('user_password', 'value'),  
+        State('confirm_password', 'value')  
     ]
 )
-
  
-def register_user(submitbtn, user_fname, user_midinitial, user_sname, user_livedname, user_sex, user_bday, user_phone_num, user_id_num, user_office, user_position, user_email, user_password, user_access_type, user_acc_status, user_profile_pic):
+
+def register_user(submitbtn, fname, mname, sname, livedname, 
+                  sex, bday, phone_num, id_num, 
+                  office, position, email, password, confirm_password):
     ctx = dash.callback_context
-    # Ensure that the function is triggered by the intended action (e.g., a button click)
-    if ctx.triggered:
-        eventid = ctx.triggered[0]['prop_id'].split('.')[0]
-        if eventid == 'registeruser_submit' and submitbtn:
-            # the submitbtn condition checks if the callback was indeed activated by a click
-            # and not by having the submit button appear in the layout
-            # Set default outputs
-            alert_open = False
-            modal_open = False
-            alert_color = ''
-            alert_text = ''
-
-            if not user_fname:
-                alert_open = True
-                alert_color = 'danger'
-                alert_text = 'Check your inputs. Please supply the first name.'
-            elif not user_sname:
-                alert_open = True
-                alert_color = 'danger'
-                alert_text = 'Check your inputs. Please supply the surname.' 
-            else:
-
-                sql = """  
-                    SELECT user_sname AS "Surname", user_fname AS "First Name", user_office AS "Dept", 
-                        user_position AS "Position", user_email AS "Email",  
-                        user_phone_num AS "Phone"
-                    VALUES (%s, %s, %s, %s, %s, %s)
-                """
-                values = [submitbtn, user_fname, user_midinitial, user_sname, user_livedname, user_sex, user_bday, user_phone_num, user_id_num, user_office, user_position, user_email, user_password, user_access_type, user_acc_status, False]
-
-                db.modifydatabase(sql, values)
-                
-                modal_open = True
-            
-            return [alert_color, alert_text, alert_open, modal_open]
-        else:  
-            raise PreventUpdate
-    else:
+    # Check if the callback was triggered by the 'save_button'
+    if not ctx.triggered:
         raise PreventUpdate
-    
+
+    eventid = ctx.triggered[0]['prop_id'].split('.')[0]
+    if eventid != 'save_button' or not submitbtn:
+        raise PreventUpdate
+
+    # Initialize the alert variables
+    alert_open = False
+    modal_open = False
+    alert_color = ''
+    alert_text = ''
+
+    # Validate password and confirmation password
+    if not password:
+        alert_open = True
+        alert_color = 'danger'
+        alert_text = 'Check your inputs. Please supply the password.'
+        return [alert_color, alert_text, alert_open, modal_open]
+
+    if not confirm_password:
+        alert_open = True
+        alert_color = 'danger'
+        alert_text = 'Check your inputs. Please supply the confirm password.'
+        return [alert_color, alert_text, alert_open, modal_open]
+
+    if password != confirm_password:
+        alert_open = True
+        alert_color = 'danger'
+        alert_text = 'Passwords do not match. Please try again.'
+        return [alert_color, alert_text, alert_open, modal_open]
+
+    # Hash the password
+    hashed_password = hash_password(password)
+
+    # Default values
+    user_access_type = 1
+    user_acc_status = 1
+    user_profile_pic = None  # This will be interpreted as NULL in SQL
+
+
+
+
+
+
+    # SQL query to insert data
+    sql = """
+        INSERT INTO maindashboard.users (
+            user_fname, user_mname, user_sname, user_livedname, 
+            user_sex, user_bday, user_phone_num, user_id_num, 
+            user_office, user_position, user_email, user_password, 
+            user_access_type, user_acc_status, user_profile_pic
+        )
+        VALUES (
+            %s, %s, %s, %s, 
+            %s, %s, %s, %s, 
+            %s, %s, %s, %s, 
+            %s, %s, %s
+        )
+    """
+
+    default_acc_status = 1   
+    values = (
+        fname, mname, sname, livedname, 
+        sex, bday, phone_num, id_num, 
+        office, position, email, hashed_password, 
+        user_access_type, user_acc_status, user_profile_pic
+    )
+
+    try: 
+        db.modifydatabase(sql, values)
+        alert_color = 'success'
+        alert_open = True
+        alert_text = "User registered successfully."
+
+    except Exception as e:
+        alert_color = 'danger'
+        alert_open = True
+        alert_text = f"Error in registration: {e}"
+
+    return [alert_color, alert_text, alert_open, modal_open]
