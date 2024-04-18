be6afd8507b72f17f683c7faa6e33c8c7b5dc8c8
prmxrjas <102643882+prmxrjas@users.noreply.github.com>
2024-04-18 16:44:40 +0800

    Update training_documents.py

--- apps/admin/training_documents.py
+++ apps/admin/training_documents.py
@@ -395,11 +395,11 @@ form = dbc.Form(
         dbc.Row(
             [
                 dbc.Col(
-                    dbc.Button("Save", color="primary", className="me-3", id="save_button", n_clicks=0),
+                    dbc.Button("Cancel", color="secondary", id="cancel_button", n_clicks=0),
                     width="auto"
                 ),
                 dbc.Col(
-                    dbc.Button("Cancel", color="secondary", id="cancel_button", n_clicks=0),
+                    dbc.Button("Save", color="primary", className="me-3", id="save_button", n_clicks=0),
                     width="auto"
                 ),
             ],
