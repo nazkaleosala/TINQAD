b1318692c38fa57f034078fec4b0a765fe18732e
Naomi Takagaki <natakagaki@up.edu.ph>
2024-05-16 11:25:13 +0800

    run

--- apps/admin/training_list.py
+++ apps/admin/training_list.py
@@ -1,8 +1,7 @@
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
-import dash_table
-import dash
+
 from dash.dependencies import Input, Output, State
 from dash.exceptions import PreventUpdate
 import pandas as pd
@@ -77,73 +76,4 @@ layout = html.Div(
     ]
 )
 
-
-
-
-@app.callback(
-    [
-        Output('traininglist_list', 'children')
-    ],
-    [
-        Input('url', 'pathname'),
-        Input('traininglist_filter', 'value'),
-    ]
-)
-def traininglist_loadlist(pathname, searchterm):
-    if pathname == '/training/training_documents':
-        sql = """
-            SELECT 
-                td.complete_name AS "QAO Name",
-                td.fac_posn AS "Faculty Position",
-                clu.cluster_name AS "Cluster",
-                col.college_name AS "College",
-                qt.trainingtype_name AS "QA Training",
-                td.departure_date AS "Departure Date",
-                td.return_date AS "Return Date",
-                td.venue AS "Venue"
-            FROM 
-                adminteam.training_documents td
-            LEFT JOIN 
-                public.clusters clu ON td.cluster_id = clu.cluster_id
-            LEFT JOIN 
-                public.college col ON td.college_id = col.college_id
-            LEFT JOIN 
-                qaofficers.training_type qt ON td.qa_training_id = qt.trainingtype_id
-        """
-
-        values = []
-
-        if searchterm:
-            like_pattern = f"%{searchterm}%"
-            additional_conditions = """
-            WHERE 
-                complete_name ILIKE %s OR
-                fac_posn ILIKE %s OR
-                cluster_id ILIKE %s OR
-                college_id ILIKE %s
-            """
-            values.extend([like_pattern, like_pattern, like_pattern, like_pattern])
-            sql += additional_conditions
-
-        final_sql = sql + " ORDER BY complete_name"
-
-        cols = [
-            "QAO Name",
-            "Faculty Position",
-            "Cluster",
-            "College",
-            "QA Training",
-            "Departure Date",
-            "Return Date",
-            "Venue"
-        ]
-
-        df = db.querydatafromdatabase(final_sql, values, cols)
-
-        if not df.empty:
-            df["Departure Date"] = df["Departure Date"].dt.strftime("%Y-%m-%d")
-            df["Return Date"] = df["Return Date"].dt.strftime("%Y-%m-%d")
-            table = dbc.Table.from_dataframe(df, striped=True, bordered=True, hover=True, size='sm')
-            return [table]
-        else:
-            return [html.Div("No records under this criteria")]
+ 
