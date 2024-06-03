78fd1548aeb1d882b44697f07ece0dcf5358257c
prmxrjas <102643882+prmxrjas@users.noreply.github.com>
2024-06-04 00:41:02 +0800

    Update search_users.py

--- apps/maindashboard/search_users.py
+++ apps/maindashboard/search_users.py
@@ -62,6 +62,9 @@ layout = html.Div(
                 ),
             ]
         ),
+        html.Br(),
+        html.Br(),
+        html.Br(),
         dbc.Row(
             [
                 dbc.Col(cm.generate_footer(), width={"size": 12, "offset": 0}),
